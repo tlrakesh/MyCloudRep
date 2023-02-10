0d522ab727346b0a6dfc5ae5914a5588c837993c
Rakesh TL <tlrakesh@gmail.com>
2023-02-10 12:11:41 -0600

    new changes

---
+++ CreateEC2Instance/Terraform_Manifests/c2.variables.tf
@@ -0,0 +1,17 @@
+variable "aws_region" {
+    description = "AWS region where ec2 instnce to be created"
+    type = string
+    default = "us-east-2"
+  
+}
+variable "instance_type" {
+    description = "Instance type of the ec2 instance"
+    type = "string"
+    default = "t2.micro"
+  
+}
+variable "instance_keypair" {
+    description = "existing key pair that needs to be associated with EC2 instance"
+    type = string
+    default = "terraform-key" 
+}
